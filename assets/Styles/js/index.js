c4b21174376596226e54c596a258ca77676902fb
AminaSalik <aminasalik012@gmail.com>
2024-07-16 17:00:12 +0100

    LoopStudios

---
+++ assets/Styles/js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('header__menu');
+    const header = document.querySelector('.header');
+    const overlay = document.querySelector('.header__overlay');
+    const links = document.querySelector('.header__links');
+
+    menu.addEventListener('click', () => {
+        header.classList.toggle('open');
+        overlay.classList.toggle('open');
+        links.classList.toggle('open');
+    });
+});
